3748b0316971d9cef637a76b693667d588bfa0a0
Owen Gruss <omg6429@rit.edu>
2017-11-27 20:17:41 -0500

    more added to server

--- src/place/server/PlaceClientThread.java
+++ src/place/server/PlaceClientThread.java
@@ -1,6 +1,14 @@
 package place.server;
 
+import java.net.Socket;
+
 public class PlaceClientThread extends Thread{
 
+    Socket connection;
+    String userName;
 
+    public PlaceClientThread(Socket connection, String userName){
+        this.connection = connection;
+        this.userName = userName;
+    }
 }
--- src/place/server/PlaceServer.java
+++ src/place/server/PlaceServer.java
@@ -1,7 +1,11 @@
 package place.server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class PlaceServer {
     public static void main(String[] args)  {
@@ -13,7 +17,10 @@ public class PlaceServer {
         int size = Integer.parseInt(args[1]);
         try(ServerSocket sS = new ServerSocket(portNum)){
             while(true){
-                new PlaceClientThread(sS.accept()).start();
+                Socket client = sS.accept();
+                BufferedReader reader = new BufferedReader(new InputStreamReader( client.getInputStream()));
+                String user = reader.readLine();
+                new PlaceClientThread(client, user).start();
             }
         }
         catch(IOException e){
