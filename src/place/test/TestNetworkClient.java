b3c74ef9d894da446292d33d5ca85a6c97ec25de
Owen Gruss <omg6429@rit.edu>
2017-11-27 19:32:18 -0500

    Test Network client

---
+++ src/place/test/TestNetworkClient.java
@@ -0,0 +1,14 @@
+package place.test;
+
+import java.net.Socket;
+
+public class TestNetworkClient {
+
+    Socket connection;
+
+    public TestNetworkClient(Socket me){
+        connection = me;
+    }
+
+
+}
--- src/place/test/TestServer.java
+++ src/place/test/TestServer.java
@@ -1,5 +1,21 @@
 package place.test;
 
-public class TestServer {
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TestServer {
+    public static void main(String args[]){
+        try{
+            ServerSocket me = new ServerSocket(1234);
+            while(true){
+                Socket client = me.accept();
+
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 }
