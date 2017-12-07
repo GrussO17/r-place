76f60d9f0c25113b40b1ac2a834d848a390b0926
pnf9945 <pnf9945@rit.edu>
2017-12-06 22:05:39 -0500

    Consolidate try blocks

--- src/place/server/PlaceServer.java
+++ src/place/server/PlaceServer.java
@@ -14,40 +14,40 @@ import java.util.Set;
 public class PlaceServer {
     private static Set<String> users = new HashSet<>();
 
-    public static void main(String[] args)  {
-        if(args.length != 2){
+    public static void main(String[] args) {
+        if (args.length != 2) {
             System.err.println("Usage: java PlaceServer <port> <size>");
             System.exit(1);
         }
         int portNum = Integer.parseInt(args[0]);
         int size = Integer.parseInt(args[1]);
         ServerModel model = new ServerModel(size);
-        try(ServerSocket serverSocket = new ServerSocket(portNum)){
-            while(true){
+        try (ServerSocket serverSocket = new ServerSocket(portNum)) {
+            while (true) {
                 Socket client = serverSocket.accept();
-                try {
-                    ObjectInputStream in = new ObjectInputStream(client.getInputStream());
-                    ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
-                    PlaceRequest loginRequest = PlaceExchange.receive(in);
-                    if (loginRequest.getType() == PlaceRequest.RequestType.LOGIN) {
-                        String username = (String)loginRequest.getData();
-                        if (!users.contains(username)) {
-                            users.add(username);
-                            new PlaceClientThread(in, out, username, model).start();
-                            System.out.println("Started thread for user " + username);
-                        } else {
-                            System.out.println("Rejected socket for user " + username);
-                        }
+
+                ObjectInputStream in = new ObjectInputStream(client.getInputStream());
+                ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
+                PlaceRequest loginRequest = PlaceExchange.receive(in);
+                if (loginRequest.getType() == PlaceRequest.RequestType.LOGIN) {
+                    String username = (String) loginRequest.getData();
+                    if (!users.contains(username)) {
+                        users.add(username);
+                        new PlaceClientThread(in, out, username, model).start();
+                        System.out.println("Started thread for user " + username);
+                    } else {
+                        System.out.println("Rejected socket for user " + username);
                     }
-                } catch (PlaceException e) {
-                    System.err.println("Failed receiving login request from new client");
                 }
+
             }
-        }
-        catch(IOException e){
+        } catch (PlaceException e) {
+            System.err.println("Failed receiving login request from new client");
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     public static void logoff(String username) {
         users.remove(username);
         System.out.println(username + " logged off");
